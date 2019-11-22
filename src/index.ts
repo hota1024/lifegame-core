38be233fc0368dba71a7db288c551991f237d07f
hota1024 <hotalog@hota1024.com>
2019-11-22 16:37:58 +0900

    :triangular_flag_on_post: Update src/index.ts

--- src/index.ts
+++ src/index.ts
@@ -5,6 +5,7 @@ export * from './Points'
 export * from './Types'
 export * from './Utilities'
 export * from './Worlds'
+export * from './WorldRenderers'
 
 import { LifeGame } from './LifeGames'
 export default LifeGame
